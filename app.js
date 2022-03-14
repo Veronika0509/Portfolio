29a9fcc7ed8834fcbc6f5d1315c3d6d2584e482e
Veronika0509 <veronikachika0509@gmail.com>
2022-03-14 16:40:35 +0300

    Preloader

--- app.js
+++ app.js
@@ -394,3 +394,14 @@ navMenuBtn.addEventListener('click', function (e) {
     navMenuBtn.classList.toggle('nav-menu__btn_active')
     navMenuList.classList.toggle('nav-menu__list_active')
 })
+
+// Preloader
+
+document.body.onload = function () {
+    setTimeout(function () {
+        const preloader = document.querySelector('.preloader')
+        if (!preloader.classList.contains('done')) {
+            preloader.classList.add('done')
+        }
+    }, 1000)
+}
